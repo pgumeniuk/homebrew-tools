c62e06ed1554bd449a2e48c9c69a408be8d2d414
Pavlo Gumeniuk <pavlo.gumeniuk@oracle.com>
2022-06-23 12:04:04 +0300

    Add openlens cask

---
+++ Casks/openlens.rb
@@ -0,0 +1,26 @@
+cask "openlens" do
+  version "5.5.4"
+
+  sha256 "8926cf2a2aa7defc2ace08544b09e017d1f58840692706cef42fb9ef52566a94"
+
+  url "https://github.com/ykursadkaya/homebrew-openlens/releases/download/v#{version}/OpenLens-#{version}.zip"
+  name "Open Lens"
+  desc "Kubernetes IDE"
+  homepage "https://k8slens.dev/"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/ykursadkaya/homebrew-openlens/main/version"
+    strategy :page_match
+    regex(/(\d+(?:\.\d+)*)/)
+  end
+
+  app "OpenLens.app"
+
+  zap trash: [
+    "~/Library/Application Support/OpenLens",
+    "~/Library/Caches/OpenLens",
+    "~/Library/Logs/OpenLens",
+    "~/Library/Preferences/com.electron.open-lens.plist",
+    "~/Library/Saved Application State/com.electron.open-lens.savedState",
+  ]
+end
